c5f2b31019aac2bfd37f1d0f211c34117b98c9b5
Kelly Schroeder <kellyrschroeder@gmail.com>
2018-07-01 17:07:59 -0600

    Adds route to get foods, needs to be namespaced still

---
+++ routes/foods.js
@@ -0,0 +1,14 @@
+const express = require("express");
+const router = express.Router();
+
+const environment = process.env.NODE_ENV || "development";
+const configuration = require("../knexfile")[environment];
+const database = require("knex")(configuration);
+
+router.get("/", function(req, res, next) {
+  database.raw("SELECT * FROM foods").then(foods => {
+    return res.status(200).json(foods.rows);
+  });
+});
+
+module.exports = router;
