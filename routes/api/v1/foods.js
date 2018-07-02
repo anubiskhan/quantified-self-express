5e20f41db0f2f141133a51c578fd838613bdd90d
Kelly Schroeder <kellyrschroeder@gmail.com>
2018-07-01 18:19:34 -0600

    Adds logic for getting a food by id

--- routes/api/v1/foods.js
+++ routes/api/v1/foods.js
@@ -15,4 +15,14 @@ router.get("/", function(req, res, next) {
   });
 });
 
+router.get("/:id", function(req, res, next) {
+  database.raw("SELECT * FROM foods WHERE id=?", req.params.id).then(food => {
+    if (food.rows.empty) {
+      return res.sendStatus(404);
+    } else {
+      return res.status(200).json(food.rows);
+    }
+  });
+});
+
 module.exports = router;
