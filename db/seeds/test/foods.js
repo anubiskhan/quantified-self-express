740df5fed1cde6d5bc18b857d004e95fc91617b7
Kelly Schroeder <kellyrschroeder@gmail.com>
2018-07-16 07:02:39 -0600

    Adds seed data for testing db

---
+++ db/seeds/test/foods.js
@@ -0,0 +1,23 @@
+exports.seed = function(knex, Promise) {
+  return knex.raw("TRUNCATE foods RESTART IDENTITY CASCADE")
+    .then(function() {
+      return Promise.all([
+        knex.raw(
+          "INSERT INTO foods (name, calories) VALUES (?, ?)",
+          ["Cabbage", 40]
+        ),
+        knex.raw(
+          "INSERT INTO foods (name, calories) VALUES (?, ?)",
+          ["Bacon", 400]
+        ),
+        knex.raw(
+          "INSERT INTO foods (name, calories) VALUES (?, ?)",
+          ["Oats", 37]
+        ),
+        knex.raw(
+          "INSERT INTO foods (name, calories) VALUES (?, ?)",
+          ["Beer", 240]
+        )
+      ]);
+    });
+};
