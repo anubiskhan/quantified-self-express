4120b017671382d3d1cac5452752091307d58203
Kelly Schroeder <kellyrschroeder@gmail.com>
2018-07-16 10:44:58 -0600

    Adds router logic for fav foods to app.js

--- app.js
+++ app.js
@@ -11,6 +11,7 @@ var indexRouter = require('./routes/index');
 var usersRouter = require('./routes/users');
 var foodsRouter = require('./routes/api/v1/foods');
 var mealsRouter = require('./routes/api/v1/meals');
+var favoriteFoodsRouter = require('./routes/api/v1/favorite_foods');
 
 // view engine setup
 app.set('views', path.join(__dirname, 'views'));
@@ -35,6 +36,7 @@ app.use('/', indexRouter);
 app.use('/users', usersRouter);
 app.use('/api/v1/foods', foodsRouter);
 app.use('/api/v1/meals', mealsRouter);
+app.use('/api/v1/favorite_foods', favoriteFoodsRouter);
 
 // catch 404 and forward to error handler
 app.use(function(req, res, next) {
