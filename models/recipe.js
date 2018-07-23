c0a982db31ff6e1619cf19648f1b649ac5ca748d
Kelly Schroeder <kellyrschroeder@gmail.com>
2018-07-22 18:22:31 -0600

    Completes iteration 3 of js assessment

--- models/recipe.js
+++ models/recipe.js
@@ -1,19 +1,27 @@
 const environment = process.env.NODE_ENV || "development";
 const configuration = require("../knexfile")[environment];
 const database = require("knex")(configuration);
-const fetch = require('node-fetch');
+const fetch = require("node-fetch");
+var pry = require("pryjs");
 
 class Recipe {
-  static async find(req) {
-    const record = await database('foods')
-      .where("id", req.params.id)
-      .first()
-    const name = record.name
-    const root = 'http://api.yummly.com/v1/api/recipes';
-    const url = `${root}?_app_id=3ab07f87&_app_key=505ffabc869d3a118cf6ff6fdea34798&q=${name}`;
-    const recipes = await fetch(url)
-
-    return recipes;
+  static find(food) {
+    const name = food.name;
+    return fetch(
+      `http://api.yummly.com/v1/api/recipes?_app_id=3ab07f87&_app_key=505ffabc869d3a118cf6ff6fdea34798&q=${name}&maxResult=10`,
+      { headers: { "Content-Type": "application/json" } }
+    )
+      .then(response => {
+        return response.json();
+      })
+      .then(recipes => {
+        return recipes.matches.map(recipe => {
+          return {
+            name: recipe.recipeName,
+            url: `http://www.yummly.com/recipe/${recipe.id}`
+          };
+        });
+      });
   }
 }
 module.exports = Recipe;
